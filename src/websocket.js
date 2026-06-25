6c51e34a11e0e282ac21a3f6cb81cb0d61494a7a
guinho1979 <guinho1979@gmail.com>
2026-06-25 03:17:36 -0300

    Add files via upload

--- src/websocket.js
+++ src/websocket.js
@@ -654,3 +654,4 @@ module.exports.setModFlags = setModFlags;
 module.exports.broadcastToRoom = broadcast;
 module.exports.broadcastAll    = broadcastAll;
 module.exports.onlineCount     = () => clients.size;
+module.exports.sendToNick      = vcSendToNick;
